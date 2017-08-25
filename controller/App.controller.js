34f386f82646593e52167bd65bce4339a1a16d4e
Xing yuan Zeng <574892028@qq.com>
2017-08-25 10:15:55 +0800

    aggregation binding using a templates

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,5 +1,5 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],
-    function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller","sap/ui/model/type/Currency"],
+    function (Controller,Currency) {
         "use strict";
         return Controller.extend("sap.ui.demo.db.controller.App",{
             formatMail:function (sFirstName,sLastName) {
@@ -9,6 +9,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
                     oBundle.getText("mailSubject",[sFirstName]),
                     oBundle.getText("mailBody")
                 );
-            }
+            },
+            formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
+            var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+            var oLocale = new sap.ui.core.Locale(sBrowserLocale);
+            var oLocaleData = new sap.ui.core.LocaleData(oLocale);
+            var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
+            return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+        }
         });
     });
